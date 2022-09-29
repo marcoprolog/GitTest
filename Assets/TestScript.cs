f35a6c4d738b9802d575cf9b0f2e0c7c06795bf6
marcoprolog <marco.prolog@gmail.com>
2022-09-29 12:02:56 +0200

    Oh no, I don't know math!

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("1+1 = 5");
+        Debug.Log("1+1 = 2");
     }
 
     // Update is called once per frame
